11eb67b26abe6171daeb8fa15983983a0a0a383e
code seven <https://github.com/codeseven777>
2026-03-17 10:22:01 -0300

    🎉 Initial Commit: Iniciando projeto.

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
+
+
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'jujuba',
+      database: 'db_farmacia',
+      entities:[],
+      synchronize: true,
+      logging: true,
+    })
+
+  
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
+ 
